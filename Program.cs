fbb136759a4c8e5c45aebd4929d1d6b1199f1869
mubaraqadenle <adenlemubaraq@gmail.com>
2023-05-26 16:31:43 +0100

    Simple Middle ware

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Middleware1
+app.Use( async (HttpContext context, RequestDelegate next) =>
+{
+  await context.Response.WriteAsync("Welcome from ASP.NET Core App!");
+  await next(context);
+});
+
+//Middleware2
+app.Use( async (HttpContext context, RequestDelegate next) =>
+{
+  await context.Response.WriteAsync("\n\n");
+  await next(context);
+});
+
+//Middleware3
+app.Run( async (HttpContext context) =>
+{
+  await context.Response.WriteAsync("This is my first ASP.NET Core App!");
+});
 
 app.Run();
